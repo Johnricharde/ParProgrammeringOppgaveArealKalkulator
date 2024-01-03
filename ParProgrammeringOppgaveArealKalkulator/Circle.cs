2e9ec6658275aac541c12ab935e5ce2a467234ef
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-03 10:24:09 +0100

    Add project files.

---
+++ ParProgrammeringOppgaveArealKalkulator/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParProgrammeringOppgaveArealKalkulator
+{
+    internal class Circle : Shape, ICalculateArea
+    {
+        public int Radius { get; set; }
+        public Circle(string name, int radius)
+        {
+            Name = name;
+            Radius = radius;
+        }
+
+        void ICalculateArea()
+        {
+        }
+    }
+}
